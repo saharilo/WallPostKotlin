826bbbe6edee7fe5b696a0321635fec0ad2b2a2d
Sah test <sahar-ilya@mail.ru>
2025-03-02 16:53:16 +0700

    functions add and update added

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -26,14 +26,28 @@ data class Likes(
 
 object WallService {
     private var posts = emptyArray<Post>()
+    private var nextId = 0
 
     fun add(post: Post): Post {
-        posts += post
+        posts += post.copy(id = nextId)
+        nextId++
         return posts.last()
+    }
+
+    fun update(post: Post): Boolean {
+        val indexPost = posts.indexOf(post)
+        if (indexPost < 0) return false
+        posts[indexPost] = post
+        return true
     }
 }
 
 fun main() {
     val post = Post(1, 1, 1, 1, 1, "Привет!", 1, 1, false)
+    val post2 = Post(0, 1, 1, 1, 1, "Привет2!", 1, 1, false)
+
+    WallService.add(post)
     println(post)
+    WallService.update(post2)
+    println(post2)
 }
