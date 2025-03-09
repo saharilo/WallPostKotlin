81f6879fd5b1d5303b90866c7921dde451cce3db
Sah test <sahar-ilya@mail.ru>
2025-03-09 21:35:19 +0700

    Classes Added

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -12,16 +12,110 @@ data class Post(
     val friendsOnly: Boolean = false,
     val original: Post? = null,
     var likes: Likes = Likes(0, false, false, false),
-) {
-
-}
+)
 
 data class Likes(
     val count: Int,
     val userLikes: Boolean,
     val canLike: Boolean,
     val canPublish: Boolean,
-) {
+)
+
+data class Sizes(
+    val type: String,
+    val url: String,
+    val width: Int,
+    val height: Int,
+)
+
+data class Images(
+    val url: String,
+    val width: Int,
+    val height: Int,
+)
+
+interface Attachment {
+    val type: String
+    val id: Int
+    val ownerId: Int
+}
+
+class Video(
+    override val type: String,
+    override val id: Int,
+    override val ownerId: Int,
+    val title: String,
+    val artist: String,
+    val url: String,
+    val lyricsId: Int,
+    val albumId: Int,
+    val genreId: Int,
+    val date: Int,
+    val duration: Int,
+    val noSearch: Boolean,
+    val isHq: Boolean
+) : Attachment {
+}
+
+class Photo(
+    override val type: String,
+    override val id: Int,
+    override val ownerId: Int,
+    val AlbumId: Int,
+    val userId: Int,
+    val text: String,
+    val date: Int,
+    val width: Int,
+    val height: Int,
+    val sizes: Array <Sizes>,
+) : Attachment {
+}
+
+class Note(
+    override val type: String,
+    override val id: Int,
+    override val ownerId: Int,
+    val title: String,
+    val text: String,
+    val date: Int,
+    val comments: Int,
+    val readComments: Int,
+    val viewUrl: String
+) : Attachment {
+}
+
+class Page(
+    override val type: String,
+    override val id: Int,
+    override val ownerId: Int,
+    val groupId: Int,
+    val creatorId: Int,
+    val title: String,
+    val currentUserCanEdit: Boolean,
+    val currentUserCanEditAccess: Boolean,
+    val whoCanView: Boolean,
+    val whoCanEdit: Boolean,
+    val edited: Int,
+    val created: Int,
+    val editorId: Int,
+    val views: Int,
+    val parent: String,
+    val parent2: String,
+    val source: String,
+    val html: String,
+    val viewUrl: String
+) : Attachment {
+}
+
+class Sticker(
+    override val type: String,
+    override val id: Int,
+    override val ownerId: Int,
+    val productId: Int,
+    val stickerId: Int,
+    var images: Array <Images>,
+    var imagesWithBackground: Array <Images>,
+): Attachment{
 
 }
 
@@ -38,7 +132,7 @@ object WallService {
 
     fun update(post: Post): Boolean {
         val (indexPost) = post
-        for((index, postItem) in posts.withIndex()){
+        for ((index, postItem) in posts.withIndex()) {
             val (postId) = postItem
             if (postId == indexPost) {
                 posts[indexPost] = post
